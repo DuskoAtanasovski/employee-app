64c6550ca2258017022da3b9c15d3b5a77e44dd1
DuskoAtanasovski <atanasovski95@yahoo.com>
2020-08-21 16:52:43 +0200

    Add existing file

--- src/main/java/com/employee/controller/EmployeeController.java
+++ src/main/java/com/employee/controller/EmployeeController.java
@@ -3,13 +3,16 @@ package com.employee.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.employee.exception.ResourceNotFoundException;
 import com.employee.model.Employee;
 import com.employee.repository.EmployeeRepository;
 
@@ -18,21 +21,27 @@ import com.employee.repository.EmployeeRepository;
 @RequestMapping("/api/v1/")
 public class EmployeeController {
 
-	@Autowired 
+	@Autowired
 	private EmployeeRepository employeeRepository;
-	
-	
-	//get all employees
+
+	// Get all employees
 	@GetMapping("/employees")
-	public List<Employee> getAllEmployees(){
+	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
 	}
-	
-	//create employee
+
+	// Create employee
 	@PostMapping("/employees")
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
 	}
-	
-	
+
+	// Get employee by ID
+	@GetMapping("/employees/{id}")
+	public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee with id: " + id + " not exist"));
+		return ResponseEntity.ok(employee);
+	}
+
 }

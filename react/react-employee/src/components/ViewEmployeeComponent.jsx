2d0b207b4132315e9540ede6b1e28a5f6c1ac6a8
DuskoAtanasovski <atanasovski95@yahoo.com>
2020-08-24 13:42:18 +0200

    Add existing file

--- react/react-employee/src/components/ViewEmployeeComponent.jsx
+++ react/react-employee/src/components/ViewEmployeeComponent.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import EmployeeService from "../services/EmployeeService";
 
 class ViewEmployeeComponent extends Component {
   constructor(props) {
@@ -6,12 +7,49 @@ class ViewEmployeeComponent extends Component {
 
     this.state = {
       id: this.props.match.params.id,
+      employee: {},
     };
   }
+
+  componentDidMount() {
+    EmployeeService.getEmployeeById(this.state.id).then((res) => {
+      this.setState({ employee: res.data });
+    });
+  }
+
   render() {
     return (
       <div>
-        <h2>View Employee Page</h2>
+        <br></br>
+        <div className="card col-md-6 offset-md-3">
+          <h3 className="text-center">View Employee Details</h3>
+          <div className="card-body">
+            <div className="row">
+              <label>
+                <h5>Employee First Name: </h5>
+              </label>
+              <div style={{ marginLeft: "5px" }}>
+                <h5>{this.state.employee.firstName}</h5>
+              </div>
+            </div>
+            <div className="row">
+              <label>
+                <h5>Employee Last Name: </h5>
+              </label>
+              <div style={{ marginLeft: "5px" }}>
+                <h5>{this.state.employee.lastName}</h5>
+              </div>
+            </div>
+            <div className="row">
+              <label>
+                <h5>Employee Email Address: </h5>
+              </label>
+              <div style={{ marginLeft: "5px" }}>
+                <h5>{this.state.employee.emailId}</h5>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
